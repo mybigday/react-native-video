f7b4acb81dfb30ce51259966303cd83a0c58c712
Jhen-Jie Hong <iainst0409@gmail.com>
2016-08-25 16:14:08 +0800

    Update ReactVideoViewManager.java

--- android/src/main/java/com/brentvatne/react/ReactVideoViewManager.java
+++ android/src/main/java/com/brentvatne/react/ReactVideoViewManager.java
@@ -1,120 +1,401 @@
 package com.brentvatne.react;
 
-import com.brentvatne.react.ReactVideoView.Events;
+import android.net.Uri;
+import android.os.Handler;
+import android.util.Log;
+import android.webkit.CookieManager;
+
 import com.facebook.react.bridge.Arguments;
-import com.devbrackets.android.exomedia.core.video.scale.ScaleType;
-import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.common.MapBuilder;
-import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 
-import javax.annotation.Nullable;
+import java.util.HashMap;
 import java.util.Map;
 
-public class ReactVideoViewManager extends SimpleViewManager<ReactVideoView> {
+import com.devbrackets.android.exomedia.ui.widget.EMVideoView;
+import com.devbrackets.android.exomedia.core.video.scale.ScaleType;
+import com.devbrackets.android.exomedia.listener.OnBufferUpdateListener;
+import com.devbrackets.android.exomedia.listener.OnCompletionListener;
+import com.devbrackets.android.exomedia.listener.OnErrorListener;
+import com.devbrackets.android.exomedia.listener.OnPreparedListener;
 
-    public static final String REACT_CLASS = "RCTVideo";
+import android.support.annotation.IntRange;
 
-    public static final String PROP_SRC = "src";
-    public static final String PROP_SRC_URI = "uri";
-    public static final String PROP_SRC_TYPE = "type";
-    public static final String PROP_SRC_IS_NETWORK = "isNetwork";
-    public static final String PROP_SRC_IS_ASSET = "isAsset";
-    public static final String PROP_RESIZE_MODE = "resizeMode";
-    public static final String PROP_REPEAT = "repeat";
-    public static final String PROP_PAUSED = "paused";
-    public static final String PROP_MUTED = "muted";
-    public static final String PROP_VOLUME = "volume";
-    public static final String PROP_SEEK = "seek";
-    public static final String PROP_RATE = "rate";
-    public static final String PROP_PLAY_IN_BACKGROUND = "playInBackground";
+public class ReactVideoView extends EMVideoView implements OnPreparedListener,
+        OnErrorListener, OnBufferUpdateListener, OnCompletionListener, LifecycleEventListener {
 
-    @Override
-    public String getName() {
-        return REACT_CLASS;
-    }
+    public enum Events {
+        EVENT_LOAD_START("onVideoLoadStart"),
+        EVENT_LOAD("onVideoLoad"),
+        EVENT_ERROR("onVideoError"),
+        EVENT_PROGRESS("onVideoProgress"),
+        EVENT_SEEK("onVideoSeek"),
+        EVENT_END("onVideoEnd"),
+        EVENT_STALLED("onPlaybackStalled"),
+        EVENT_RESUME("onPlaybackResume"),
+        EVENT_READY_FOR_DISPLAY("onReadyForDisplay");
 
-    @Override
-    protected ReactVideoView createViewInstance(ThemedReactContext themedReactContext) {
-        return new ReactVideoView(themedReactContext);
-    }
+        private final String mName;
 
-    @Override
-    @Nullable
-    public Map getExportedCustomDirectEventTypeConstants() {
-        MapBuilder.Builder builder = MapBuilder.builder();
-        for (ReactVideoView.Events event : ReactVideoView.Events.values()) {
-            builder.put(event.toString(), MapBuilder.of("registrationName", event.toString()));
+        Events(final String name) {
+            mName = name;
         }
-        return builder.build();
+
+        @Override
+        public String toString() {
+            return mName;
+        }
+    }
+
+    public static final String EVENT_PROP_FAST_FORWARD = "canPlayFastForward";
+    public static final String EVENT_PROP_SLOW_FORWARD = "canPlaySlowForward";
+    public static final String EVENT_PROP_SLOW_REVERSE = "canPlaySlowReverse";
+    public static final String EVENT_PROP_REVERSE = "canPlayReverse";
+    public static final String EVENT_PROP_STEP_FORWARD = "canStepForward";
+    public static final String EVENT_PROP_STEP_BACKWARD = "canStepBackward";
+
+    public static final String EVENT_PROP_DURATION = "duration";
+    public static final String EVENT_PROP_PLAYABLE_DURATION = "playableDuration";
+    public static final String EVENT_PROP_CURRENT_TIME = "currentTime";
+    public static final String EVENT_PROP_SEEK_TIME = "seekTime";
+    public static final String EVENT_PROP_NATURALSIZE = "naturalSize";
+    public static final String EVENT_PROP_WIDTH = "width";
+    public static final String EVENT_PROP_HEIGHT = "height";
+    public static final String EVENT_PROP_ORIENTATION = "orientation";
+
+    public static final String EVENT_PROP_ERROR = "error";
+    public static final String EVENT_PROP_WHAT = "what";
+    public static final String EVENT_PROP_EXTRA = "extra";
+
+    private ThemedReactContext mThemedReactContext;
+    private RCTEventEmitter mEventEmitter;
+
+    private Handler mProgressUpdateHandler = new Handler();
+    private Runnable mProgressUpdateRunnable = null;
+
+    private String mSrcUriString = null;
+    private String mSrcType = "mp4";
+    private boolean mSrcIsNetwork = false;
+    private boolean mSrcIsAsset = false;
+    private ScaleType mResizeMode = ScaleType.CENTER_INSIDE;
+    private boolean mRepeat = false;
+    private boolean mPaused = false;
+    private boolean mMuted = false;
+    private float mVolume = 1.0f;
+    private float mRate = 1.0f;
+    private boolean mPlayInBackground = false;
+
+    private boolean mMediaPlayerValid = false; // True if mMediaPlayer is in prepared, started, paused or completed state.
+    private int mVideoDuration = 0;
+    private int mVideoBufferedDuration = 0;
+    private boolean isCompleted = false;
+    private boolean isBuffering = false;
+    private boolean lifecyclePaused = false;
+
+    public ReactVideoView(ThemedReactContext themedReactContext) {
+        super(themedReactContext);
+
+        mThemedReactContext = themedReactContext;
+        mEventEmitter = themedReactContext.getJSModule(RCTEventEmitter.class);
+        themedReactContext.addLifecycleEventListener(this);
+
+        setupPlayerListeners();
+
+        mProgressUpdateRunnable = new Runnable() {
+            @Override
+            public void run() {
+
+                if (mMediaPlayerValid && !isCompleted && (!mPaused || isBuffering)) {
+                    WritableMap event = Arguments.createMap();
+                    event.putDouble(EVENT_PROP_CURRENT_TIME, getCurrentPosition() / 1000.0);
+                    event.putDouble(EVENT_PROP_PLAYABLE_DURATION, mVideoBufferedDuration / 1000.0); //TODO:mBufferUpdateRunnable
+                    mEventEmitter.receiveEvent(getId(), Events.EVENT_PROGRESS.toString(), event);
+                }
+                mProgressUpdateHandler.postDelayed(mProgressUpdateRunnable, 250);
+            }
+        };
+        mProgressUpdateHandler.post(mProgressUpdateRunnable);
+    }
+
+    private void setupPlayerListeners() {
+//        mMediaPlayer.setOnVideoSizeChangedListener(this);
+        this.setOnErrorListener(this);
+        this.setOnPreparedListener(this);
+        this.setOnBufferUpdateListener(this);
+        this.setOnCompletionListener(this);
+//        mMediaPlayer.setOnInfoListener(this);
+    }
+
+    public void setSrc(final String uriString, final String type, final boolean isNetwork, final boolean isAsset) {
+
+        mSrcUriString = uriString;
+        mSrcType = type;
+        mSrcIsNetwork = isNetwork;
+        mSrcIsAsset = isAsset;
+
+        mMediaPlayerValid = false;
+        mVideoDuration = 0;
+        mVideoBufferedDuration = 0;
+
+        try {
+            if (isNetwork) {
+                // Use the shared CookieManager to access the cookies
+                // set by WebViews inside the same app
+                CookieManager cookieManager = CookieManager.getInstance();
+
+                Uri parsedUrl = Uri.parse(uriString);
+                Uri.Builder builtUrl = parsedUrl.buildUpon();
+
+                String cookie = cookieManager.getCookie(builtUrl.build().toString());
+
+                Map<String, String> headers = new HashMap<String, String>();
+
+                if (cookie != null) {
+                    headers.put("Cookie", cookie);
+                }
+
+                setVideoURI(parsedUrl);
+            } else if (isAsset) {
+                if (uriString.startsWith("content://")) {
+                    Uri parsedUrl = Uri.parse(uriString);
+                    setVideoURI(parsedUrl);
+                } else {
+                    setVideoPath(uriString);
+                }
+            } else {
+                // TODO: raw resource is not supported by ExoPlayer, use assets folder instead!
+                throw new IllegalArgumentException("raw resource not supported by ExoPlayer, use assets folder and path like \"asset:///video.mp4\" instead!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        WritableMap src = Arguments.createMap();
+        src.putString(ReactVideoViewManager.PROP_SRC_URI, uriString);
+        src.putString(ReactVideoViewManager.PROP_SRC_TYPE, type);
+        src.putBoolean(ReactVideoViewManager.PROP_SRC_IS_NETWORK, isNetwork);
+        WritableMap event = Arguments.createMap();
+        event.putMap(ReactVideoViewManager.PROP_SRC, src);
+        mEventEmitter.receiveEvent(getId(), Events.EVENT_LOAD_START.toString(), event);
+    }
+
+    public void setResizeModeModifier(final ScaleType resizeMode) {
+        mResizeMode = resizeMode;
+
+        if (mMediaPlayerValid) {
+            setScaleType(resizeMode);
+            invalidate();
+        }
+    }
+
+    public void setRepeatModifier(final boolean repeat) {
+
+        mRepeat = repeat;
+
+// TODO: no looping support from ExoPlayer yet, so do it manually at complete event
+//        if (mMediaPlayerValid) {
+//            setLooping(repeat);
+//        }
+    }
+
+    public void setPausedModifier(final boolean paused) {
+
+        mPaused = paused;
+
+        if (!mMediaPlayerValid) {
+            return;
+        }
+
+        if (mPaused) {
+            if (this.isPlaying()) {
+                pause();
+            }
+        } else {
+            if (!this.isPlaying()) {
+                start();
+            }
+        }
+    }
+
+    public void setMutedModifier(final boolean muted) {
+        mMuted = muted;
+
+        if (!mMediaPlayerValid) {
+            return;
+        }
+
+        if (mMuted) {
+            setVolume(0);
+        } else {
+            setVolume(mVolume);
+        }
+    }
+
+    public void setVolumeModifier(final float volume) {
+        mVolume = volume;
+        setMutedModifier(mMuted);
+    }
+
+    public void setRateModifier(final float rate) {
+        mRate = rate;
+
+        if (mMediaPlayerValid) {
+            // TODO: Implement this.
+            Log.e(ReactVideoViewManager.REACT_CLASS, "Setting playback rate is not yet supported on Android");
+        }
+    }
+
+    public void applyModifiers() {
+        setResizeModeModifier(mResizeMode);
+// TODO: no looping support from ExoPlayer yet, so do it manually at complete event
+//        setRepeatModifier(mRepeat);
+        setPausedModifier(mPaused);
+        setMutedModifier(mMuted);
+//        setRateModifier(mRate);
+    }
+
+    public void setPlayInBackground(final boolean playInBackground) {
+
+        mPlayInBackground = playInBackground;
     }
 
     @Override
-    @Nullable
-    public Map getExportedViewConstants() {
-        return MapBuilder.of(
-                "ScaleNone", Integer.toString(ScaleType.NONE.ordinal()),
-                "ScaleToFill", Integer.toString(ScaleType.FIT_CENTER.ordinal()),
-                "ScaleAspectFit", Integer.toString(ScaleType.FIT_CENTER.ordinal()),
-                "ScaleAspectFill", Integer.toString(ScaleType.CENTER_CROP.ordinal())
-        );
+    public void onPrepared() {
+
+        mMediaPlayerValid = true;
+        mVideoDuration = getDuration();
+
+        WritableMap naturalSize = Arguments.createMap();
+        naturalSize.putInt(EVENT_PROP_WIDTH, getWidth());
+        naturalSize.putInt(EVENT_PROP_HEIGHT, getHeight());
+        if (getWidth() > getHeight())
+            naturalSize.putString(EVENT_PROP_ORIENTATION, "landscape");
+        else
+            naturalSize.putString(EVENT_PROP_ORIENTATION, "portrait");
+
+        WritableMap event = Arguments.createMap();
+        event.putDouble(EVENT_PROP_DURATION, mVideoDuration / 1000.0);
+        event.putDouble(EVENT_PROP_CURRENT_TIME, getCurrentPosition() / 1000.0);
+        event.putMap(EVENT_PROP_NATURALSIZE, naturalSize);
+        // TODO: Actually check if you can.
+        event.putBoolean(EVENT_PROP_FAST_FORWARD, true);
+        event.putBoolean(EVENT_PROP_SLOW_FORWARD, true);
+        event.putBoolean(EVENT_PROP_SLOW_REVERSE, true);
+        event.putBoolean(EVENT_PROP_REVERSE, true);
+        event.putBoolean(EVENT_PROP_FAST_FORWARD, true);
+        event.putBoolean(EVENT_PROP_STEP_BACKWARD, true);
+        event.putBoolean(EVENT_PROP_STEP_FORWARD, true);
+        mEventEmitter.receiveEvent(getId(), Events.EVENT_LOAD.toString(), event);
+
+        applyModifiers();
     }
 
-    @ReactProp(name = PROP_SRC)
-    public void setSrc(final ReactVideoView videoView, @Nullable ReadableMap src) {
-        videoView.setSrc(
-                src.getString(PROP_SRC_URI),
-                src.getString(PROP_SRC_TYPE),
-                src.getBoolean(PROP_SRC_IS_NETWORK),
-                src.getBoolean(PROP_SRC_IS_ASSET)
-        );
+    @Override
+    public boolean onError() {
+
+        WritableMap error = Arguments.createMap();
+        error.putInt(EVENT_PROP_WHAT, 0);
+        error.putInt(EVENT_PROP_EXTRA, 0);
+        WritableMap event = Arguments.createMap();
+        event.putMap(EVENT_PROP_ERROR, error);
+        mEventEmitter.receiveEvent(getId(), Events.EVENT_ERROR.toString(), event);
+        return true;
     }
 
-    @ReactProp(name = PROP_RESIZE_MODE)
-    public void setResizeMode(final ReactVideoView videoView, final String resizeModeOrdinalString) {
-        videoView.setResizeModeModifier(ScaleType.values()[Integer.parseInt(resizeModeOrdinalString)]);
+    // @Override
+    // public boolean onInfo(MediaPlayer mp, int what, int extra) {
+    //     switch (what) {
+    //         case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+    //             mEventEmitter.receiveEvent(getId(), Events.EVENT_STALLED.toString(), Arguments.createMap());
+    //             break;
+    //         case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+    //             mEventEmitter.receiveEvent(getId(), Events.EVENT_RESUME.toString(), Arguments.createMap());
+    //             break;
+    //         case MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START:
+    //             mEventEmitter.receiveEvent(getId(), Events.EVENT_READY_FOR_DISPLAY.toString(), Arguments.createMap());
+    //             break;
+
+    //         default:
+    //     }
+    //     return false;
+    // }
+
+    @Override
+    public void onBufferingUpdate(@IntRange(from = 0, to = 100) int percent) {
+        mVideoBufferedDuration = (int) Math.round((double) (mVideoDuration * percent) / 100.0);
+        if (percent == 100) {
+            isBuffering = false;
+        } else if (!isBuffering) {
+            isBuffering = true;
+        }
     }
 
-    @ReactProp(name = PROP_REPEAT, defaultBoolean = false)
-    public void setRepeat(final ReactVideoView videoView, final boolean repeat) {
-        videoView.setRepeatModifier(repeat);
+    @Override
+    public void seekTo(int msec) {
+
+        if (mMediaPlayerValid) {
+            WritableMap event = Arguments.createMap();
+            event.putDouble(EVENT_PROP_CURRENT_TIME, getCurrentPosition() / 1000.0);
+            event.putDouble(EVENT_PROP_SEEK_TIME, msec / 1000.0);
+            mEventEmitter.receiveEvent(getId(), Events.EVENT_SEEK.toString(), event);
+
+            super.seekTo(msec);
+            if (isCompleted && mVideoDuration != 0 && msec < mVideoDuration) {
+                isCompleted = false;
+            }
+        }
     }
 
-    @ReactProp(name = PROP_PAUSED, defaultBoolean = false)
-    public void setPaused(final ReactVideoView videoView, final boolean paused) {
-        videoView.setPausedModifier(paused);
+    @Override
+    public void onCompletion() {
+
+        isCompleted = true;
+        mEventEmitter.receiveEvent(getId(), Events.EVENT_END.toString(), null);
+        if (mRepeat) {
+            // TODO: no looping support from ExoPlayer yet, so do it manually here
+            setSrc(mSrcUriString, mSrcType, mSrcIsNetwork, mSrcIsAsset);
+        } else {
+            mMediaPlayerValid = false;
+            mEventEmitter.receiveEvent(getId(), Events.EVENT_END.toString(), null);
+        }
     }
 
-    @ReactProp(name = PROP_MUTED, defaultBoolean = false)
-    public void setMuted(final ReactVideoView videoView, final boolean muted) {
-        videoView.setMutedModifier(muted);
+    @Override
+    protected void onDetachedFromWindow() {
+
+        mMediaPlayerValid = false;
+        super.onDetachedFromWindow();
     }
 
-    @ReactProp(name = PROP_VOLUME, defaultFloat = 1.0f)
-    public void setVolume(final ReactVideoView videoView, final float volume) {
-        videoView.setVolumeModifier(volume);
+    @Override
+    protected void onAttachedToWindow() {
+
+        super.onAttachedToWindow();
+        setSrc(mSrcUriString, mSrcType, mSrcIsNetwork, mSrcIsAsset);
     }
 
-    @ReactProp(name = PROP_SEEK)
-    public void setSeek(final ReactVideoView videoView, final float seek) {
-        videoView.seekTo(Math.round(seek * 1000.0f));
+    @Override
+    public void onHostPause() {
+        if (!mPlayInBackground) {
+            pause();
+            lifecyclePaused = true;
+            mPaused = true;
+        }
     }
 
-    @ReactProp(name = PROP_RATE)
-    public void setRate(final ReactVideoView videoView, final float rate) {
-        videoView.setRateModifier(rate);
+    @Override
+    public void onHostResume() {
+        if (lifecyclePaused && !this.isPlaying()) {
+            start();
+            lifecyclePaused = false;
+            mPaused = false;
+        }
     }
 
-    @ReactProp(name = PROP_PLAY_IN_BACKGROUND, defaultBoolean = false)
-    public void setPlayInBackground(final ReactVideoView videoView, final boolean playInBackground) {
-        videoView.setPlayInBackground(playInBackground);
-    }
-
-    @ReactProp(name = "release", defaultBoolean = false)
-    public void releaseIt(final ReactVideoView videoView, final boolean r) {
-        videoView.release();
+    @Override
+    public void onHostDestroy() {
     }
 }
