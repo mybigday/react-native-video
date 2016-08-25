6c505758c19c221ed1b334649a5d2e58c354cb17
Jhen-Jie Hong <iainst0409@gmail.com>
2016-08-25 15:31:39 +0800

    Update ReactVideoView.java

--- android/src/main/java/com/brentvatne/react/ReactVideoView.java
+++ android/src/main/java/com/brentvatne/react/ReactVideoView.java
@@ -85,6 +85,8 @@ public class ReactVideoView extends ScalableVideoView implements MediaPlayer.OnP
     private int mVideoDuration = 0;
     private int mVideoBufferedDuration = 0;
     private boolean isCompleted = false;
+    private boolean isBuffering = false;
+    private boolean lifecyclePaused = false;
 
     public ReactVideoView(ThemedReactContext themedReactContext) {
         super(themedReactContext);
@@ -100,7 +102,7 @@ public class ReactVideoView extends ScalableVideoView implements MediaPlayer.OnP
             @Override
             public void run() {
 
-                if (mMediaPlayerValid && !isCompleted) {
+                if (mMediaPlayerValid && !isCompleted && (!mPaused || isBuffering)) {
                     WritableMap event = Arguments.createMap();
                     event.putDouble(EVENT_PROP_CURRENT_TIME, mMediaPlayer.getCurrentPosition() / 1000.0);
                     event.putDouble(EVENT_PROP_PLAYABLE_DURATION, mVideoBufferedDuration / 1000.0); //TODO:mBufferUpdateRunnable
@@ -331,6 +333,11 @@ public class ReactVideoView extends ScalableVideoView implements MediaPlayer.OnP
     @Override
     public void onBufferingUpdate(MediaPlayer mp, int percent) {
         mVideoBufferedDuration = (int) Math.round((double) (mVideoDuration * percent) / 100.0);
+        if (percent == 100) {
+            isBuffering = false;
+        } else if (!isBuffering) {
+            isBuffering = true;
+        }
     }
 
     @Override
@@ -375,11 +382,19 @@ public class ReactVideoView extends ScalableVideoView implements MediaPlayer.OnP
 
         if (mMediaPlayer != null && !mPlayInBackground) {
             mMediaPlayer.pause();
+            pause();
+            lifecyclePaused = true;
+            mPaused = true;
         }
     }
 
     @Override
     public void onHostResume() {
+        if (lifecyclePaused && mMediaPlayer != null && !mMediaPlayer.isPlaying()) {
+            start();
+            lifecyclePaused = false;
+            mPaused = false;
+        }
     }
 
     @Override
